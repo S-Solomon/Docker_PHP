f0dbfea48bf384a91145ec2a44a86a1520cb15a8
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-04-30 19:34:58 +0100

    first commit

---
+++ src/public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '../Transactions.php';
+
+// CLASSES AND OBJECTS
+$transaction1 = (new Transaction(100, 'Transaction 1'))
+    ->addTax(8)
+    ->applyDiscount(10);
+    
+$transaction2 = (new Transaction(200, 'Transaction 2'))
+    ->addTax(8)
+    ->applyDiscount(15);
+
+
+    
+var_dump($transaction1->getAmount(), $transaction2->getAmount());
