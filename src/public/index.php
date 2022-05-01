6ec1df3f82bf79b27998380de4604ed2bd63d370
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-05-01 08:00:40 +0100

    namespace, use keyword & AS alias

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 require_once '../Transactions.php';
 
+use PaymentGateway\Stripe\Transaction;
+
 // CLASSES AND OBJECTS
 $amount = (new Transaction(100, 'Transaction 1'))
     ->addTax(8)
