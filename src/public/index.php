4da7fb1bca08b66f5b306c8c21e026df9f24bda4
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-05-02 21:33:11 +0100

    $_Server superglobal

--- src/public/index.php
+++ src/public/index.php
@@ -5,15 +5,10 @@
 // require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
 // require_once '../app/PaymentGateway/Paddle/Transaction.php';
 
-use App\PaymentGateway\Paddle\Transaction;
+// use App\PaymentGateway\Paddle\Transaction;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// $transaction = new Transaction(25);
-// $transaction->process();
-
-$dateTime = new DateTime();
-echo $dateTime->getTimezone()->getName() . ' - ' . $dateTime->format('m/d/Y g:i A'). PHP_EOL;
-
-$dateTime->setTimezone(new DateTimeZone('Europe/London'));
-echo $dateTime->getTimezone()->getName() . ' - ' . $dateTime->format('m/d/Y g:i A'). PHP_EOL;
+echo '<pre>'; 
+print_r($_SERVER);
+echo '</pre>';
