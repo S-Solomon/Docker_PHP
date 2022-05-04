181d71d8f017105b4819a9c139b9af86fdb9721b
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-05-04 09:21:54 +0100

    php mvc pattern

--- src/public/index.php
+++ src/public/index.php
@@ -9,20 +9,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+session_start();
+
+define('STORAGE_PATH', __DIR__ . '/../storage');
+define('VIEW_PATH', __DIR__ . '/../views');
+
 $router = new App\Router();
 
-$router->register(
-    '/',
-    function() {
-        echo 'Home';
-    }
-);
 
-$router->register(
-    '/invoices',
-    function() {
-        echo 'Invoices';
-    }
-);
 
 echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
